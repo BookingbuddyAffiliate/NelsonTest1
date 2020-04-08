6ac5e457e6f7becb01a0db511e8a271b75eaf17f
nelsoneng34 <61510103+nelsoneng34@users.noreply.github.com>
2020-04-08 13:42:55 -0400

    Update rwc-pass-parameters.js

--- rwc-pass-parameters.js
+++ rwc-pass-parameters.js
@@ -1,29 +1,4 @@
-/**
- * This script passes URL parameters to iframes with specified ids
- *
- * @author Road Warrior Creative
- * @link https://roadwarriorcreative.com
- * @package RWC_Pass_Parameters_iframe
- * @version 1.0.1
- * @license GPL-2.0+
- */
- 
- jQuery(function($){
-	$(window).load(function() {
-		var iframe = document.getElementById("rwcGetParams");
-		if (document.contains(iframe)) {
-			var loc = window.location.toString(),
-					params = loc.split('?')[1],
-					query;
-			if (params){
-				if( iframe.src.indexOf('?') >= 0){
-					query = '&';
-				}else{
-					query = '?';
-				}
-				newsrc = iframe.src + query + params;
-				$('#rwcGetParams').attr('src', newsrc);
-			}
-		}
-	});
+$(function() {
+    var search = window.location.search;
+    $("#myiframe").attr("src", $("#myiframe").attr("src")+search);
 });
